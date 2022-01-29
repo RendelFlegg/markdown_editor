df93be9708d6f22c24e72dc25407ccf42693d2ef
RendelFlegg <62382457+RendelFlegg@users.noreply.github.com>
2022-01-29 16:00:26 +0500

    Completed stage 3: Implemented formatters

--- Markdown Editor/task/editor.py
+++ Markdown Editor/task/editor.py
@@ -1,21 +1,71 @@
-formatters = ['plain', 'bold', 'italic', 'header', 'ordered-list', 'unordered-list', 'link', 'inline-code', 'new-line']
+formatters = ['plain', 'bold', 'italic', 'header', 'link', 'inline-code', 'new-line']
 commands = ['!help', '!done']
 
+
+def plain():
+    pass
+
+
+def bold(text):
+    return f'**{text}**'
+
+
+def italic(text):
+    return f'*{text}*'
+
+
+def header(level, text):
+    return f'{"#" * int(level)} {text}\n'
+
+
+def link(label, url):
+    return f'[{label}]({url})'
+
+
+def inline_code(text):
+    return f'`{text}`'
+
+
+# def new_line(text):
+#     return f'{text}\n'
+
+
+def choose_formatter(formatter):
+    if formatter == 'plain':
+        text_list.append(input('Text: '))
+    elif formatter == 'bold':
+        text_list.append(bold(input('Text: ')))
+    elif formatter == 'italic':
+        text_list.append(italic(input('Text: ')))
+    elif formatter == 'header':
+        while True:
+            try:
+                header_level = int(input('Level: '))
+                assert header_level in range(1, 7)
+            except (ValueError, AssertionError):
+                print('The level should be within the range of 1 to 6')
+            else:
+                text_list.append(header(header_level, input('Text: ')))
+                break
+    elif formatter == 'link':
+        text_list.append(link(input('Label: '), input('URL: ')))
+    elif formatter == 'inline-code':
+        text_list.append(inline_code(input('Text: ')))
+    elif formatter == 'new-line':
+        text_list.append('\n')
+
+
+text_list = []
 while True:
-    answer = input('- Choose a formatter:')
+    answer = input('- Choose a formatter: ')
     if answer not in formatters + commands:
         print('Unknown formatting type or command.')
-    if answer == '!help':
-        print(f'Available formatters: {" ".join(formatters)}')
-        print(f"Special commands: {' '.join(commands)}")
-    elif answer == '!done':
-        break
-
-# print("# John Lennon")
-# print("or ***John Winston Ono Lennon*** was one of *The Beatles*.")
-# print("Here are the songs he wrote I like the most:")
-# print("* Imagine")
-# print("* Norwegian Wood")
-# print("* Come Together")
-# print("* In My Life")
-# print("* ~~Hey Jude~~ (that was *McCartney*)")
+    elif answer in commands:
+        if answer == '!help':
+            print(f'Available formatters: {" ".join(formatters)}')
+            print(f"Special commands: {' '.join(commands)}")
+        elif answer == '!done':
+            break
+    else:
+        choose_formatter(answer)
+        print(''.join(text_list))
